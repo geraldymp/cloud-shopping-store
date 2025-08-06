847da4523d0a194b5053f6b4bd05ad5a2f270645
Geraldy Martin <geraldymp24@gmail.com>
2025-08-06 11:54:01 +0700

    Initial Commit

---
+++ Cloud Shopping Store/Cloud_Shopping_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloud_Shopping_StoreApp.swift
+//  Cloud Shopping Store
+//
+//  Created by Geraldy on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Cloud_Shopping_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloud Shopping Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cloud Shopping Store
+//
+//  Created by Geraldy on 06/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
